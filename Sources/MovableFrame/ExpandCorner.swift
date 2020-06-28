7e178811da94508b1cb37974621426ee3852cf16
Tomoaki Yagishita <tyagishi@gmail.com>
2020-06-28 21:12:45 +0900

    add files

---
+++ Sources/MovableFrame/ExpandCorner.swift
@@ -0,0 +1,159 @@
+//
+//  ExpandCorner.swift
+//  MovableFrame
+//
+//  Created by Tomoaki Yagishita on 2020/06/08.
+//  Copyright © 2020 SmallDeskSoftware. All rights reserved.
+//
+
+import SwiftUI
+
+struct ExpandCorner: View {
+    @Binding var frameRect: CGRect
+    @Binding var canvasRect: CGRect
+    @Binding var isHovering:Bool
+    let dragBoxSize: CGSize
+    let fixedCorner: FrameView.Anchor
+    @State var isDraggingUL: Bool = false
+    @State var dragStartRect: CGRect = CGRect.zero
+
+    var body: some View {
+        let dragCorner = ExpandCorner.oppositeCorner(corner: fixedCorner)
+        let cornerCenter = ExpandCorner.cornerPosition(frame: frameRect, corner: dragCorner)
+        let cornerCenterInSize = CGSize(width: cornerCenter.x - canvasRect.width / 2, height: cornerCenter.y - canvasRect.height / 2)
+        return Rectangle()
+            .fill(Color.clear)
+            .contentShape(Rectangle())
+            .frame(width: dragBoxSize.width, height: dragBoxSize.height)
+            .onHover(perform: { isIn in
+                isHovering = isIn
+                print("isIn(ExpandCorner): \(isIn)")
+//                isIn ? NSCursor.resizeUpDown.set() : NSCursor.arrow.set()
+                isIn ? self.cursor() : NSCursor.arrow.set()
+            })
+            .border(Color.blue)
+            .offset(cornerCenterInSize)
+            .gesture(DragGesture()
+                .onChanged { gesture in
+                    if self.isDraggingUL == false {
+                        self.dragStartRect = self.frameRect
+                        self.isDraggingUL = true
+                    }
+                    // change size
+                    let moveSize = ExpandCorner.getMoveSize(rect: self.frameRect.size, translation: gesture.translation, fixedCorner: self.fixedCorner)
+                    let checkRect = ExpandCorner.changeSizeWithFixedCorner( rect: self.dragStartRect, moveInX: moveSize, anchor: self.fixedCorner)
+                    if self.canvasRect.contains(checkRect) {
+                        self.frameRect = checkRect
+                    }
+                }
+                .onEnded { gesture in
+                    self.isDraggingUL = false
+                    NSCursor.arrow.set()
+                })
+    }
+    
+    func cursor() -> Void {
+//        let cursorDir = "resizenortheastsouthwest"
+//        let cursorFile = "cursor.pdf"
+//        let baseURL = URL(fileURLWithPath: "/System/Library/Frameworks/ApplicationServices.framework/Versions/A/Frameworks/HIServices.framework/Versions/A/Resources/cursors")
+//        let cursorBasePath = baseURL.appendingPathComponent(cursorDir)
+//        let cursorImagePath = cursorBasePath.appendingPathComponent(cursorFile)
+//        let cursorInfoPath = cursorBasePath.appendingPathComponent("info.plist")
+//        let nsImage = NSImage(byReferencingFile: cursorImagePath.absoluteString)!
+//        let cursorInfo = NSDictionary(contentsOfFile: cursorInfoPath.absoluteString)
+//        let hotPoint = NSPoint(x: cursorInfo?.value(forKey: "hotx") as! Double, y: cursorInfo?.value(forKey: "hoty") as! Double)
+//        let cursor = NSCursor.init(image: nsImage, hotSpot: hotPoint)
+        let cursor = NSCursor(image: NSImage(byReferencingFile: "/System/Library/Frameworks/WebKit.framework/Versions/Current/Frameworks/WebCore.framework/Resources/northWestSouthEastResizeCursor.png")!, hotSpot: NSPoint(x: 8, y: 8))
+        cursor.set()
+    }
+    
+    static func oppositeCorner(corner: FrameView.Anchor) -> FrameView.Anchor {
+        switch corner {
+        case .UpperLeft:
+            return .LowerRight
+        case .UpperRight:
+            return .LowerLeft
+        case .LowerLeft:
+            return .UpperRight
+        case .LowerRight:
+            return .UpperLeft
+        }
+    }
+    
+    static func cornerPosition(frame: CGRect, corner: FrameView.Anchor) -> CGPoint {
+        switch corner {
+        case .UpperLeft:
+            return CGPoint(x: frame.minX, y: frame.minY)
+        case .UpperRight:
+            return CGPoint(x: frame.maxX, y: frame.minY)
+        case .LowerLeft:
+            return CGPoint(x: frame.minX, y: frame.maxY)
+        case .LowerRight:
+            return CGPoint(x: frame.maxX, y: frame.maxY)
+        }
+    }
+
+    public static func getMoveSize(rect: CGSize, translation: CGSize, fixedCorner: FrameView.Anchor) -> CGFloat {
+        let yDivX = rect.height / rect.width
+        let x = translation.width
+        let y = translation.height * yDivX
+
+        var moveSize:CGFloat = 0
+        switch fixedCorner {
+        case .LowerRight:
+            moveSize = abs(x) > abs(y) ? x * -1 : y * -1 // moveSize   +: expand   -: schrink
+        case .LowerLeft:
+            moveSize = abs(x) > abs(y) ? x * +1 : y * -1 // moveSize   +: expand   -: schrink
+        case .UpperRight:
+            moveSize = abs(x) > abs(y) ? x * -1 : y * +1 // moveSize   +: expand   -: schrink
+        case .UpperLeft:
+            moveSize = abs(x) > abs(y) ? x * +1 : y * +1 // moveSize   +: expand   -: schrink
+        }
+        return moveSize
+    }
+    public  static func changeSizeWithFixedCorner( rect: CGRect, moveInX: CGFloat, anchor: FrameView.Anchor) -> CGRect {
+        let yDivX =  rect.size.height / rect.size.width
+        let changeInX = moveInX
+        let changeInY = (yDivX*moveInX)
+
+        //    origin
+        //   (A)--------(B)
+        //    |          |
+        //    |          |
+        //    |          |
+        //    |          |
+        //   (C)--------(D)
+        if anchor == .LowerRight {
+            // keep D
+            let newSize = CGSize(width: rect.size.width + changeInX, height: rect.size.height + changeInY)
+            let newOrigin = CGPoint(x: rect.origin.x - changeInX, y: rect.origin.y - changeInY)
+            return CGRect(origin: newOrigin, size: newSize)
+        }
+        if anchor == .LowerLeft {
+            // keep C
+            let newSize = CGSize(width: rect.size.width + changeInX, height: rect.size.height + changeInY)
+            let newOrigin = CGPoint(x: rect.origin.x, y: rect.origin.y - changeInY)
+            return CGRect(origin: newOrigin, size: newSize)
+        }
+        if anchor == .UpperRight {
+            // keep B
+            let newSize = CGSize(width: rect.size.width + changeInX, height: rect.size.height + changeInY)
+            let newOrigin = CGPoint(x: rect.origin.x - changeInX, y: rect.origin.y)
+            return CGRect(origin: newOrigin, size: newSize)
+        }
+        if anchor == .UpperLeft {
+            // keep A
+            let newSize = CGSize(width: rect.size.width + changeInX, height: rect.size.height + changeInY)
+            let newOrigin = CGPoint(x: rect.origin.x, y: rect.origin.y)
+            return CGRect(origin: newOrigin, size: newSize)
+        }
+        return CGRect(origin: CGPoint.zero, size: CGSize.zero)
+    }
+}
+
+struct ExpandCorner_Previews: PreviewProvider {
+    static var previews: some View {
+        ExpandCorner(frameRect: .constant(CGRect.zero), canvasRect: .constant(CGRect.zero), isHovering: .constant(false),
+                     dragBoxSize: CGSize(width: 10, height: 10), fixedCorner: .LowerLeft)
+    }
+}
---
+++ Sources/MovableFrame/FrameView.swift
@@ -0,0 +1,128 @@
+//
+//  FrameView.swift
+//  MovableFrame
+//
+//  Created by Tomoaki Yagishita on 2020/05/20.
+//  Copyright © 2020 SmallDeskSoftware. All rights reserved.
+//
+
+import SwiftUI
+
+struct FrameView: View {
+    public enum Anchor {
+        case UpperLeft, UpperRight, LowerLeft, LowerRight
+    }
+
+    @Binding var frameRect: CGRect
+    @Binding var canvasRect: CGRect
+    @State private var isDragging:Bool = false
+    @State private var dragStartRect: CGRect = CGRect.zero
+    @State private var isHoveringOnCorner = false
+    var cornerDragBoxSize = CGSize(width: 50, height: 50)
+
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .stroke(lineWidth: 3).foregroundColor(Color.red)
+                .contentShape(Rectangle().inset(by: -10))
+                .onHover(perform: { isIn in
+                    if isHoveringOnCorner { return }
+                    isIn ? NSCursor.openHand.set() : NSCursor.arrow.set()
+                })
+                .tlPlacement(rect: frameRect)
+                .border(Color.orange)
+                .gesture(dragMoveGesture)
+                .overlay(ExpandCorner(frameRect: $frameRect, canvasRect: $canvasRect, isHovering: $isHoveringOnCorner,
+                                      dragBoxSize: cornerDragBoxSize, fixedCorner: .LowerRight))
+                .overlay(ExpandCorner(frameRect: $frameRect, canvasRect: $canvasRect, isHovering: $isHoveringOnCorner,
+                                      dragBoxSize: cornerDragBoxSize, fixedCorner: .LowerLeft))
+                .overlay(ExpandCorner(frameRect: $frameRect, canvasRect: $canvasRect, isHovering: $isHoveringOnCorner,
+                                      dragBoxSize: cornerDragBoxSize, fixedCorner: .UpperRight))
+                .overlay(ExpandCorner(frameRect: $frameRect, canvasRect: $canvasRect, isHovering: $isHoveringOnCorner,
+                                      dragBoxSize: cornerDragBoxSize, fixedCorner: .UpperLeft))
+            Circle()
+            .frame(width: 10, height: 10)
+                .border(Color.red, width: 5)
+                .position(x: canvasRect.width/2, y: canvasRect.height/2)
+        }
+    }
+
+    var dragMoveGesture: some Gesture {
+        DragGesture()
+            .onChanged { gesture in
+                if self.isDragging == false {
+                    self.dragStartRect = self.frameRect
+                    self.isDragging = true
+                }
+
+                let newPosition = CGPoint(x: gesture.translation.width + self.dragStartRect.origin.x,
+                                          y: gesture.translation.height + self.dragStartRect.origin.y)
+                let checkRect = CGRect(origin: newPosition, size: self.frameRect.size)
+                if self.canvasRect.contains(checkRect) {
+                    self.frameRect.origin = newPosition
+                }
+            }
+            .onEnded { gesture in
+                self.isDragging = false
+                NSCursor.arrow.set()
+            }
+    }
+}
+
+extension CGRect {
+    // return CGSize for .offset to place Rectangle
+    func offsetForRect() -> CGSize {
+        return CGSize(width: self.width/2, height: self.height/2)
+    }
+}
+
+
+struct FrameView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameView(frameRect: .constant(CGRect.zero), canvasRect: .constant(CGRect.zero))
+    }
+}
+
+struct TopLeadingPlacement: ViewModifier {
+    let rect: CGRect
+    func body(content: Content) -> some View {
+            content
+                .frame(width: rect.size.width, height: rect.size.height)
+//                .offset(x: rect.origin.x + rect.size.width/2, y: rect.origin.y + rect.size.height/2)
+                .position(x: rect.origin.x + rect.size.width/2,
+                          y: rect.origin.y + rect.size.height/2)
+    }
+}
+
+extension View {
+    func tlPlacement( rect: CGRect ) -> some View {
+        self.modifier(TopLeadingPlacement(rect: rect))
+    }
+}
+
+struct FrameViewRectPreferenceData: Equatable {
+    let name: String
+    let rect: CGRect
+}
+
+struct FrameViewRectPreferenceKey: PreferenceKey {
+    typealias Value = [FrameViewRectPreferenceData]
+    
+    static var defaultValue:[FrameViewRectPreferenceData] = []
+    
+    static func reduce(value: inout [FrameViewRectPreferenceData], nextValue: () -> [FrameViewRectPreferenceData]) {
+        value.append(contentsOf: nextValue())
+    }
+}
+
+struct FrameViewRectPreferenceSetter: View {
+    let prefName: String
+    var body: some View {
+        GeometryReader { geom in
+            Rectangle()
+                .fill(Color.clear)
+                .preference(key: FrameViewRectPreferenceKey.self,
+                            value: [FrameViewRectPreferenceData(name: self.prefName, rect: geom.frame(in: .named("mainZStack")))])
+        }
+    }
+}
